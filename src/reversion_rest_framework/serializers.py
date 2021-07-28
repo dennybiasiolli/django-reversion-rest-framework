4b4b1f4e9457cf2e32527824eb57df39478de36b
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2021-07-28 16:51:48 +0200

    releasing a first working version of the tool

---
+++ src/reversion_rest_framework/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from reversion.models import Revision, Version
+
+
+class RevisionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Revision
+        fields = ('date_created', 'user', 'comment')
+
+
+class VersionSerializer(serializers.ModelSerializer):
+    revision = RevisionSerializer(read_only=True)
+
+    class Meta:
+        model = Version
+        fields = ('revision', 'field_dict',)
