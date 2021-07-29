c3de69485489b874ad7bb760efcda139376d1f09
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-07-29 15:40:34 +0200

    viewsets: allowing users to customize the version_serializer

--- tests/test_app/serializers.py
+++ tests/test_app/serializers.py
@@ -1,4 +1,10 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from reversion.models import Revision, Version
+from reversion_rest_framework.serializers import (
+    RevisionSerializer,
+    VersionSerializer,
+)
 
 from .models import TestModel
 
@@ -7,3 +13,21 @@ class TestModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = TestModel
         fields = ['id', 'name']
+
+
+class UserSerializer(serializers.Serializer):
+
+    class Meta:
+        model = User
+        fields = ['username']
+
+
+class CustomRevisionSerializer(RevisionSerializer):
+    user = UserSerializer(
+        read_only=True,
+        default=serializers.CurrentUserDefault(),
+    )
+
+
+class CustomVersionSerializer(VersionSerializer):
+    revision = CustomRevisionSerializer(read_only=True)
