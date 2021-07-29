04017972f811985924c8cebc8118de403532ee45
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2021-07-29 11:38:07 +0200

    tests: adding base test for reversion

--- tests/test_app/tests/test_api.py
+++ tests/test_app/tests/test_api.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from reversion.models import Version
 
 from test_app.models import TestModel
 
@@ -11,8 +12,48 @@ class TestModelTests(APITestCase):
         Ensure we have history after creating a TestModel object.
         """
         url = reverse('testmodel-list')
-        data = {'name': 'Foo 1.0.0'}
+        data = {'name': 'Foo 1.1.0'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(TestModel.objects.count(), 1)
-        self.assertEqual(TestModel.objects.get().name, 'Foo 1.0.0')
+        test_model_1 = TestModel.objects.get()
+        self.assertEqual(test_model_1.name, 'Foo 1.1.0')
+        versions = Version.objects.get_for_object(test_model_1)
+        self.assertEqual(len(versions), 1)
+
+    def test_editing_test_model(self):
+        """
+        Ensure we have history after editing a TestModel object.
+        """
+        url = reverse('testmodel-list')
+        data = {'name': 'Foo 1.2.0'}
+        response = self.client.post(url, data, format='json')
+
+        test_model_1 = TestModel.objects.get()
+        url = reverse('testmodel-detail', kwargs={'pk': test_model_1.pk})
+        data = {'name': 'Foo 1.2.1'}
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        test_model_1 = TestModel.objects.get()
+        self.assertEqual(test_model_1.name, 'Foo 1.2.1')
+        versions = Version.objects.get_for_object(test_model_1)
+        self.assertEqual(len(versions), 2)
+
+    def test_deleting_test_model(self):
+        """
+        Ensure we have deletion history after deleting a TestModel object.
+        """
+        url = reverse('testmodel-list')
+        data = {'name': 'Foo 1.2.0'}
+        response = self.client.post(url, data, format='json')
+
+        test_model_1 = TestModel.objects.get()
+        url = reverse('testmodel-detail', kwargs={'pk': test_model_1.pk})
+        data = {'name': 'Foo 1.2.1'}
+        response = self.client.delete(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        versions_qs = Version.objects.get_deleted(TestModel)
+        versions = versions_qs.filter(pk=test_model_1.pk)
+        self.assertEqual(len(versions), 1)
