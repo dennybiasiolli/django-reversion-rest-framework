155a91e3599677de4443943b2102c5ae381c07f1
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2021-07-29 11:24:36 +0200

    tests: adding and configuring reversion in the test_project

--- tests/test_project/settings.py
+++ tests/test_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'reversion',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'reversion.middleware.RevisionMiddleware',
 ]
 
 ROOT_URLCONF = 'test_project.urls'
