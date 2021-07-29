e7d0db7f0d2297659feb0f574c6e8a7917918a12
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2021-07-29 11:07:06 +0200

    tests: configuring rest_framework

--- tests/test_app/views.py
+++ tests/test_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from .models import TestModel
+from .serializers import TestModelSerializer
+
+
+class TestModelViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = TestModel.objects.all()
+    serializer_class = TestModelSerializer
+    permission_classes = [permissions.IsAuthenticated]
