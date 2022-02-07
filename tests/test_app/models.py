d52043f990ff221e5104ff7efa81d30144bdaa06
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-02-07 12:26:20 +0100

    mixins: returning the base `field_dict` in case of errors during serialization
    This happens when trying to serialize an history object with only a selected
    list of fields.

--- tests/test_app/models.py
+++ tests/test_app/models.py
@@ -10,3 +10,9 @@ class TestModel(models.Model):
 @reversion.register()
 class TestParentModel(models.Model):
     children = models.ManyToManyField(TestModel)
+
+
+@reversion.register(fields=('name',))
+class TestLimitedModel(models.Model):
+    name = models.CharField(max_length=10)
+    description = models.CharField(max_length=100)
